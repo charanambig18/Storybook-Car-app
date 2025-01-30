373a154e914a3d727445e130540d05e9056ceff4
charanambig18 <ambigcharan2006@gmail.com>
2025-01-30 10:22:14 +0530

    commit changes

--- react/src/Component/stories 1/Task.jsx
+++ react/src/Component/stories 1/Task.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import "./Task.css";
 import heart from "./images/heart.jpg";
-import heartPink from "./images/heart-pink.png";
+import orange from "./images/Like-orange.png";
 import location from "./images/location.png";
+import whatsapp from "./images/whatsapp.png"
+import share from "./images/share.png"
 
 const carImages = [
   "https://images10.gaadi.com/usedcar_image/4320910/original/10e495689e38c63ed57b8bc8a0a08fee.JPG?imwidth=640",
+  "https://images10.gaadi.com/usedcar_image/4320910/original/2576637a8940cdad3c87c57f64dbfe32.JPG?imwidth=640",
+  "https://images10.gaadi.com/usedcar_image/4320910/original/7e22d2a745f023d31cf1c79ecbd81924.JPG?imwidth=640",
   "https://images10.gaadi.com/usedcar_image/4320910/original/1d0e7a56f0ee267546ee430089c711fa.JPG?imwidth=640",
   "https://images10.gaadi.com/usedcar_image/4320910/original/93a7ae7062fdd0b1618bc5d5feca6423.JPG?imwidth=640",
+  "https://images10.gaadi.com/usedcar_image/4320910/original/99219699e6edf84def34785989357399.JPG?imwidth=640",
+  "https://images10.gaadi.com/usedcar_image/4320910/original/ef9fbdad4594456520593e9eb7809fd8.JPG?imwidth=640",
   "https://images10.gaadi.com/usedcar_image/4320910/original/dbe0deb27e9e4024c583e79a233e5637.JPG?imwidth=640",
 ];
 
@@ -44,13 +50,14 @@ export default function Task() {
 
       {/* Right Side (Static Car Details) */}
       <div className="inner2">
+        <div className="top">
         <div className="inner2-1">
           <div className="in1">
             <h2>2023 Maruti Alto K10 VXI Plus</h2>
             <button>CERTIFIED</button>
 
             <img
-              src={isFavorited ? heartPink : heart}
+              src={isFavorited ? orange : heart}
               alt="Favorite"
               className={`heart-icon ${isFavorited ? "favorited" : ""}`}
               onClick={toggleFavorite}
@@ -82,6 +89,22 @@ export default function Task() {
         <div className="inner2-2">
           <h3>Trending Car!</h3>
           <p>High chances of sale in next 6 days</p>
+        </div>
+        </div>
+        <div className="bottom">
+          <div className="app-icon">
+            <img src={whatsapp} alt="" />
+            <div className="app-des">
+            <a href="">Chat with Seller</a>
+            </div>
+          </div>
+
+          <div className="app-icon">
+            <img src={share} alt="" />
+            <div className="app-des">
+            <a href="">Share</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
