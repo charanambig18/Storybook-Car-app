167cb2d3926a7a20790d73a9ff62d9a6f8e96eac
charanambig18 <ambigcharan2006@gmail.com>
2025-02-12 13:12:24 +0530

    changed

--- react/src/Component/AceCoinPay/AceCoinPay.jsx
+++ react/src/Component/AceCoinPay/AceCoinPay.jsx
@@ -1,112 +1,168 @@
-import React from 'react'
-import "./AceCoinPay.css"
-// import chipImg from "../assets/chip.png";
-// import wifiImg from "../assets/wifi.png";
-// import mcSymbol from "../assets/mc_symbol.svg";
-// import verifiedBadge from "../assets/verified-badge.svg";
-// import dotsImg from "../assets/dots.svg";
-// import appleLogo from "../assets/apple-13.svg";
-// import mastercardLogo from "../assets/mastercard-2.svg";
-// import docketImg from "../assets/docket.png";
+import React from 'react';
+import './AceCoinPay.css';
 
-const AceCoinPay = () => {
+import AceCoinlogo from "./images/Acecoin-logo.png"
+import pencil from "./images/pencil.png"
+import mcSymbol from "./images/masterCard-Logo.png"
+import verifiedBadge from "./images/verified.png"
+// import dotsImg from "./images/dots-menu.png"
+import chipImg from "./images/chip.png";
+import wifiImg from "./images/wifi.png";
+import mastercardLogo from "./images/mastercard.png"
+import appleLogo from "./images/apple.png"
+import docketImg from "./images/docket.png"
+
+
+const Example = () => {
   return (
-    <div className="wrapper grid grid-cols-12">
-      <div className="close-btn">&times;</div>
-      <div className="col-span-12 md:col-span-8">
-        <div className="form">
-          <header className="form-header">
-            <div className="logo">
-              <span className="logo-circle"></span>
-              <h1>AceCoin<span className="lean">Pay</span></h1>
+    <div className="wrapper">
+      <div className="close-btn">
+        <img src={AceCoinlogo} alt="Close Icon" />
+      </div>
+      <div className="form">
+        <div className="form-header">
+          <div className="logo">
+            <div className="logo-circle" />
+            <h1>
+              Ace<span className="lean">Coin</span>
+            </h1>
+          </div>
+          <div className="timer">
+            <span className="timer-box">0</span>
+            <span className="timer-box">1</span>
+            <span className="timer-divider">:</span>
+            <span className="timer-box">1</span>
+            <span className="timer-box">9</span>
+          </div>
+        </div>
+
+        <div className="form-body">
+          <form>
+            <div className="input-group">
+              <div className="header">
+                <div className="description">
+                  <div className="title">Card Number</div>
+                  <div className="desc">Enter the 16-digit card number on the card</div>
+                </div>
+                <button className="edit">
+                  <img src={pencil} alt="Edit Icon" />
+                  <span className="btn-desc">Edit</span>
+                </button>
+              </div>
+              <div className="main">
+                <img src={mcSymbol} alt="Card Leading Icon" className="leading-icon" />
+                <input type="text" placeholder="2414-7512-4312-3456" />
+                <img src={verifiedBadge} alt="Card Trailing Icon" className="trailing-icon" />
+              </div>
             </div>
-            <div className="timer">01:19</div>
-          </header>
-          <section className="form-body">
-            <form>
-              <div className="input-group">
-                <div className="header">
-                  <h2 className="title">Card Number</h2>
-                  <p className="desc">Enter the 15-digit card number on the card</p>
-                </div>
-                <div className="main">
-                  <img src={mcSymbol} alt="" className="leading-icon" />
-                  <input type="text" placeholder="2412 - 7512 - 3412 - 3456" />
-                  <img src={verifiedBadge} alt="" className="trailing-icon" />
+
+            <div className="input-group">
+              <div className="header">
+                <div className="description">
+                  <div className="title">CVV Number</div>
+                  <div className="desc">Enter the 3 or 4 digit number on the card</div>
                 </div>
               </div>
-              <div className="input-group __col">
-                <h2 className="title">CVV Number</h2>
-                <p className="desc">Enter the 3 or 4 digit number on the card</p>
-                <div className="main">
-                  <input type="text" placeholder="327" />
-                  <img src={dotsImg} alt="" className="trailing-icon" />
+              <div className="main">
+                <input type="password" placeholder="327" />
+              </div>
+            </div>
+
+            <div className="input-group __col">
+              <div className="header">
+                <div className="description">
+                  <div className="title">Expiry Date</div>
+                  <div className="desc">Enter the expiration date of the card</div>
                 </div>
               </div>
-              <div className="input-group __col">
-                <h2 className="title">Expiry Date</h2>
-                <p className="desc">Enter the expiration date of the card</p>
-                <div className="grp">
-                  <input type="text" placeholder="09" className="__date" />
-                  <span className="divider">/</span>
-                  <input type="text" placeholder="22" className="__date" />
+              <div className="main __date">
+                <input type="text" placeholder="MM" />
+              </div>
+              <div className="divider">/</div>
+              <div className="main __date">
+                <input type="text" placeholder="YY" />
+              </div>
+            </div>
+
+            <div className="input-group">
+              <div className="header">
+                <div className="description">
+                  <div className="title">Password</div>
+                  <div className="desc">Enter your dynamic password</div>
                 </div>
               </div>
-              <div className="input-group __col">
-                <h2 className="title">Password</h2>
-                <p className="desc">Enter your Dynamic password</p>
-                <div className="main">
-                  <input type="password" placeholder="******" />
-                  <img src={dotsImg} alt="" className="trailing-icon" />
-                </div>
+              <div className="main">
+                <input type="password" placeholder="Password" />
               </div>
-              <button type="submit" className="btn-submit">Pay Now</button>
-            </form>
-          </section>
+            </div>
+
+            <button type="submit" className="btn-submit">Pay Now</button>
+          </form>
         </div>
       </div>
-      <div className="col-span-12 md:col-span-4">
-        <div className="checkout">
-          <div className="credit-card">
-            <div className="credit-card__head">
-              <img src={chipImg} alt="" className="chip" />
-              <img src={wifiImg} alt="" className="wifi" />
-            </div>
-            <div className="credit-card__body">
-              <span className="holder">Jonathan Michael</span>
-              <div className="card-digits">**** **** **** 3456</div>
+
+      <div className="checkout">
+        <div className="credit-card">
+          <div className="credit-card__head">
+            <img src={chipImg} alt="Card Logo" />
+            <img src={wifiImg} alt="Card Chip" className="chip" />
+          </div>
+          <div className="credit-card__body">
+            <span className="holder">Card Holder</span>
+            <div className="card-digits">
+              <div className="hidden-digits">
+                <span className="circle" />
+                <span className="circle" />
+                <span className="circle" />
+                <span className="circle" />
+              </div>
+              <span className="visible-digits">1234</span>
             </div>
             <div className="credit-card__footer">
-              <span className="expiry">09/22</span>
-              <img src={mastercardLogo} alt="" className="logo" />
+              <span className="expiry">09/24</span>
+              <img src={mastercardLogo} alt="Visa Logo" className="logo" />
             </div>
           </div>
-          <div className="checkout-details">
-            <div className="purchase-data">
-              <div className="data">
-                <span className="title">Company</span>
-                <span className="value">
-                  <img src={appleLogo} alt="" />
-                  <span>Apple</span>
-                </span>
-              </div>
-              <div className="data">
-                <span className="title">Order Number</span>
-                <span className="value">1266201</span>
-              </div>
-              <div className="data">
-                <span className="title">Product</span>
-                <span className="value">MacBook Air</span>
-              </div>
-              <div className="data">
-                <span className="title">VAT (20%)</span>
-                <span className="value">$100.00</span>
+          <div className="card__design">
+            <div className="inner" />
+          </div>
+        </div>
+
+        <div className="checkout-details">
+          <div className="purchase-data">
+            <div className="data">
+              <span className="title">Company</span>
+              <div className="value">
+                <img src={appleLogo} alt="Apple Logo" />
+                <span className="inner-text">Apple</span>
               </div>
             </div>
-            <div className="purchase-total">
-              <h1 className="amount">549<sub>.99</sub> USD</h1>
-              <img src={docketImg} alt="" />
+            <div className="data">
+              <span className="title">Order Number</span>
+              <span className="inner-text">123456</span>
             </div>
+            <div className="data">
+              <span className="title">Product</span>
+              <span className="inner-text">MacBook Air</span>
+            </div>
+            <div className="data">
+              <span className="title">VAT</span>
+              <span className="inner-text">$100.00</span>
+            </div>
+          </div>
+          <div className="divider">
+            <div className="left" />
+            <div className="right" />
+          </div>
+          <div className="purchase-total">
+            <div className="total">
+              <span className="desc">Total</span>
+              <div className="t-amount">
+                <span className="amount">$1200</span>
+                <sub>USD</sub>
+              </div>
+            </div>
+            <img src={docketImg} alt="Card Logo" />
           </div>
         </div>
       </div>
@@ -114,4 +170,4 @@ const AceCoinPay = () => {
   );
 };
 
-export default AceCoinPay
+export default Example;
