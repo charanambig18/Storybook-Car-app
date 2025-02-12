192cc4c1a8d8b9ca27696758e1781ddc59ea83dd
charanambig18 <ambigcharan2006@gmail.com>
2025-02-12 10:18:09 +0530

    changed

--- react/src/Component/AceCoinPay/AceCoinPay.stories.jsx
+++ react/src/Component/AceCoinPay/AceCoinPay.stories.jsx
@@ -1,43 +1,11 @@
-import { fn } from "@storybook/test";
 import AceCoinPay from "./AceCoinPay";
 
 export default {
-  component: AceCoinPay,
   title: "Componant/AceCoinPay",
-  tags: ["autodocs"],
-  excludeStories: /.*Data$/,
-  args: {
-    onArchiveTask: fn(),
-    onPinTask: fn(),
-  },
+  component: AceCoinPay,
 };
 
-export const Default = {
-  args: {
-    task: {
-      id: "1",
-      title: "Test Task",
-      state: "TASK_INBOX",
-    },
-  },
-};
+const Template = (args) => <AceCoinPay {...args} />;
 
-export const Pinned = {
-  args: {
-    task: {
-      id: "1",
-      title: "Test Task",
-      state: "TASK_PINNED",
-    },
-  },
-};
-
-export const Archived = {
-  args: {
-    task: {
-      id: "1",
-      title: "Test Task",
-      state: "TASK_ARCHIVED",
-    },
-  },
-};
+export const Default = Template.bind({});
+Default.args = {};
