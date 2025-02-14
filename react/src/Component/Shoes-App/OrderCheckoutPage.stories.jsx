5d5c5537a1c7929332e7cb56cc731d698441cdd1
charanambig18 <ambigcharan2006@gmail.com>
2025-02-14 09:13:15 +0530

    changed

---
+++ react/src/Component/Shoes-App/OrderCheckoutPage.stories.jsx
@@ -0,0 +1,12 @@
+
+import OrderCheckoutPage from "./OrderCheckoutPage";
+
+export default {
+  title: "Componant/OrderCheckoutPage",
+  component: OrderCheckoutPage,
+};
+
+const Template = (args) => <OrderCheckoutPage {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
